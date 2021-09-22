8a3b514cc784fd228d9f115d4cd606329c15aaa8
Joey Steele <jsteele@gatech.edu>
2021-09-22 16:29:32 -0400

    Initial react / typescript setup

---
+++ app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <main>
+      <h1>Hello World!</h1>
+      <p>This is the start of our app!</p>
+    </main>
+  );
+}
+
+export default App;
