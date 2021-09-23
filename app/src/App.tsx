545ff499e563c5d6002e7f51b465f33465e44d0c
Joey Steele <jsteele@gatech.edu>
2021-09-23 10:23:31 -0400

    Write basic youtube player component

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -6,8 +6,15 @@ const App = () => {
     <main>
       <h1>Hello World!</h1>
       <p>This is the start of our app!</p>
+      <YouTubePlayer />
     </main>
   );
 }
+
+const YouTubePlayer = () => {
+  return (
+    <iframe id="music-vid-player" title="Never Gonna Give You Up" src="https://www.youtube.com/embed/dQw4w9WgXcQ" />
+  );
+};
 
 export default App;
