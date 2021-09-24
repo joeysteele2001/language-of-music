fdfed7356cc3c445d412c5696ec052c23f95ef73
Joey Steele <jsteele@gatech.edu>
2021-09-24 10:25:52 -0400

    Make basic page routing

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,18 +1,39 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 import YouTubePlayer from './components/YouTubePlayer';
+import Home from './components/Home';
 
 const App = () => {
   return (
-    <main>
-      <h1>Hello World!</h1>
-      <p>This is the start of our app!</p>
-      <YouTubePlayer
-        id="music-video-player"
-        title="Music Video Player"
-        videoId="dQw4w9WgXcQ"
-      />
-    </main>
+    <BrowserRouter>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/rickroll">Rickroll</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route exact path="/rickroll">
+          <YouTubePlayer
+            id="music-video-player"
+            title="Music Video Player"
+            videoId="dQw4w9WgXcQ"
+          />
+        </Route>
+      </Switch>
+
+
+    </BrowserRouter>
   );
 }
 
