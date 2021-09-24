1b734b98f483f5ccf520ef197cc5e5a269c51789
Jessmae27 <90634769+Jessmae27@users.noreply.github.com>
2021-09-24 10:28:41 -0400

    Add Select Mode components

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
 import YouTubePlayer from './components/YouTubePlayer';
+import SelectMode from './components/SelectMode';
 
 const App = () => {
   return (
-    <main>
+    <main className = "App">
       <h1>Hello World!</h1>
       <p>This is the start of our app!</p>
       <YouTubePlayer
@@ -12,8 +13,11 @@ const App = () => {
         title="Music Video Player"
         videoId="dQw4w9WgXcQ"
       />
+      <nav className = 'SelectMode'>
+        <a href='./components/SelectMode'>Select Mode</a>
+      </nav>
     </main>
   );
-}
+};
 
 export default App;
---
+++ app/src/components/SelectMode.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './SelectMode.css'
+
+const SelectMode = () => {
+return (
+    <main>
+    <h1>Select Mode</h1>
+    <div className= "SelectMode">
+        <div>Just Listen</div>
+        <div>Listen and Learn</div>
+        <div>Play Along</div>
+    </div>
+    </main>
+);
+};
+
+
+export default SelectMode;
