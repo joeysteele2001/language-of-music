7d28beb7b5fc0f53cca5507debe581fc5312167c
Joey Steele <jsteele@gatech.edu>
2021-09-27 18:19:03 -0400

    Test out lyrics component

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,22 +2,6 @@ import React from 'react';
 import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 import YouTubePlayer from './components/YouTubePlayer';
-<<<<<<< HEAD
-import Lyrics from './components/Lyrics';
-
-const App = () => {
-  return (
-    <main>
-      <h1>Hello World!</h1>
-      <p>This is the start of our app!</p>
-      <YouTubePlayer
-        id="music-video-player"
-        title="Music Video Player"
-        videoId="dQw4w9WgXcQ"
-      />
-      <Lyrics />
-    </main>
-=======
 import Home from './components/Home';
 
 const App = () => {
@@ -50,7 +34,6 @@ const App = () => {
 
 
     </BrowserRouter>
->>>>>>> master
   );
 }
 
