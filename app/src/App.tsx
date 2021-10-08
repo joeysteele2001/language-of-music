39674f9774fcc0b90bb5ec2da7435d793cbe0335
Joey Steele <jsteele@gatech.edu>
2021-10-08 10:52:28 -0400

    Integrate Switcher into App

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,46 +1,46 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 import JustListen from './components/JustListen';
 import Home from './components/Home';
 import Quiz from './components/Quiz';
 
+import Switcher from './components/Switcher';
+
 const App = () => {
+  const [page, setPage] = React.useState(0);
+
   return (
-    <BrowserRouter>
+    <main>
       <nav>
         <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/justlisten">Just Listen</Link>
-          </li>
-          <li>
-            <Link to="/quiz">Quiz</Link>
-          </li>
+          {pages.map((page, idx) => (
+            <li key={page.name}>
+              <button className="button-link" onClick={() => setPage(idx)}>
+                {page.name}
+              </button>
+            </li>
+          ))}
         </ul>
       </nav>
 
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-
-        <Route exact path="/justlisten">
-          <JustListen />
-        </Route>
-
-        <Route exact path="/quiz">
-          <Quiz
-            question="Question"
-            answers={["Answer 1", "Answer 2", "Answer 3", "I'm the correct answer", "Answer 5"]}
-            correctAnswer={3}
-          />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+      <Switcher activeView={page}>
+        {pages.map(page => page.component)}
+      </Switcher>
+    </main >
   );
 }
+
+const pages = [
+  { name: "Home", component: <Home /> },
+  { name: "Just Listen", component: <JustListen /> },
+  {
+    name: "Quiz",
+    component: <Quiz
+      question="Question"
+      answers={["Answer 1", "Answer 2", "Answer 3", "I'm the correct answer", "Answer 5"]}
+      correctAnswer={3}
+    />
+  },
+];
 
 export default App;
