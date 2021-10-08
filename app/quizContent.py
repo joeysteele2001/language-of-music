faf0d5bb75cfad82a8604342e7313a423d869955
JessPaley <jessicayelap@gmail.com>
2021-10-08 10:40:20 -0400

    generated one vocab list

--- app/quizContent.py
+++ app/quizContent.py
@@ -2,16 +2,18 @@
 from csv import reader
 
 def vocabSearch(song="uneditedLyrics.html", level="n5copy.csv"):
-    with open(level) as myFile, open(song) as mySong:
+    with open(level) as myFile, open(song) as mySong, open('foundVocab.csv','w') as output:
         vocab = reader(myFile)
         lyrics = mySong.read()
         outList = []
+        counter = 0
         for i in vocab:
             if i[0] in lyrics:
-                outList += {i[2]: i[0]}
+                output.write(i[0] + ', ' + i[2] + "\n")
+                counter += 1
                 continue
             
-    print(outList)
+    
 
 
 
