dd47d64be0a9ff21f4c27bd39c90dc0286b7e373
JessPaley <jessicayelap@gmail.com>
2021-10-13 11:51:04 -0400

    All vocab lists, ruby output

--- app/quizContent.py
+++ app/quizContent.py
@@ -1,20 +1,21 @@
 # -*- coding: utf-8 -*-
 from csv import reader
 
-def vocabSearch(song="uneditedLyrics.html", level="n5copy.csv"):
-    with open(level) as myFile, open(song) as mySong, open('foundVocab.csv','w') as output:
+def vocabSearch(song="uneditedLyrics.html", level="n5.csv"):
+    with open(level) as myFile, open(song) as mySong, open('foundVocab.html','w') as output:
         vocab = reader(myFile)
         lyrics = mySong.read()
         outList = []
         counter = 0
         for i in vocab:
             if i[0] in lyrics:
-                output.write(i[0] + ', ' + i[2] + "\n")
+                if i[0] == i[1]:
+                    output.write('<ruby>' + i[0] + '</ruby>' + ', ' + i[2] + "\n")
+                else: 
+                    output.write('<ruby><rb>' + i[0] + '</rb><rp>(</rp><rt>' + i[1] + '</rt><rp>)</rp></ruby>' + ', ' + i[2] + "\n")
                 counter += 1
                 continue
             
-    
-#use original csv to generate ruby tags?
 
 
 
