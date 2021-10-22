ecf9142770d79601bc8097fa4be7209455fae3fd
Joey Steele <jsteele@gatech.edu>
2021-10-22 09:08:26 -0400

    Song browser and select mode styling

--- app/src/components/Home.tsx
+++ app/src/components/Home.tsx
@@ -14,34 +14,46 @@ const Home = () => {
 
             <div className="SongBrowser">
                 <div className="SongBrowser-song">
-                    <Link to="#">Song 1</Link>
-                </div>
-
-                <div
-                    className="SongBrowser-song"
-                    style={{
-                        backgroundImage: "url(https://img.youtube.com/vi/_IkopJwRDKU/mqdefault.jpg)",
-                        backgroundRepeat: "no-repeat",
-                        backgroundPosition: "center",
-                        backgroundSize: "cover",
-                    }}
-                >
-                    <Link to={`${url}/SelectMode`}>Gurenge</Link>
+                    <Link to="#">
+                        <div className="SongBrowser-thumbnail"></div>
+                        <div className="SongBrowser-name">Song 1</div>
+                    </Link>
                 </div>
 
                 <div className="SongBrowser-song">
-                    <Link to="#">Song 3</Link>
+                    <Link to={`${url}/SelectMode`}>
+                        <div
+                            className="SongBrowser-thumbnail"
+                            style={{
+                                backgroundImage: "url(https://img.youtube.com/vi/_IkopJwRDKU/mqdefault.jpg)",
+                                backgroundRepeat: "no-repeat",
+                                backgroundPosition: "center",
+                                backgroundSize: "cover",
+                            }}
+                        />
+                        <div className="SongBrowser-name">Gurenge</div>
+                    </Link>
                 </div>
 
                 <div className="SongBrowser-song">
+                    <Link to="#">
+                        <div className="SongBrowser-thumbnail"></div>
+                        <div className="SongBrowser-name">Song 3</div>
+                    </Link>
+                </div>
+
+                <div className="SongBrowser-song">
+                    <div className="SongBrowser-thumbnail"></div>
                     <Link to="#">Song 4</Link>
                 </div>
 
                 <div className="SongBrowser-song">
+                    <div className="SongBrowser-thumbnail"></div>
                     <Link to="#">Song 5</Link>
                 </div>
 
                 <div className="SongBrowser-song">
+                    <div className="SongBrowser-thumbnail"></div>
                     <Link to="#">Song 6</Link>
                 </div>
             </div>
