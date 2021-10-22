3b2c8d79fdb815ba3e6ef7637d2a1a021376788d
JessPaley <jessicayelap@gmail.com>
2021-10-22 10:38:18 -0400

    Generate questions and incorrect answer choices

--- app/randomizeQuestions.py
+++ app/randomizeQuestions.py
@@ -2,7 +2,9 @@
 from random import sample
 
 # takes in a song's vocab list file and creates a file with a unique subset 
-# of those vocab words (default 5 words) to display in the quiz
+# of questions (default 5) to display in the quiz
+# the first line is the correct answer, followed by incorrect answer choices
+# only Japanese is provided for incorrect answers
 def randomizeQuestions(file, numberOfQuestions=5):
     with open(file) as myList, open('questionList.html', 'w') as output:
         lines = myList.readlines()
@@ -11,17 +13,29 @@ def randomizeQuestions(file, numberOfQuestions=5):
         indices = sample(range(0, len(lines)), numberOfQuestions)
         for i in indices:
             output.write(lines[i])
-            # incorrectAnswers(file, i)
+            incorrectIndices = incorrectAnswers(file, i, lines)
+            for j in incorrectIndices:
+                output.write(lines[j].split()[0][:-1] + '\n')
+            output.write('\n')
 
-
-# todo: generate incorrect answers
+# helper function that generates incorrect answer choice indices with no repeats
 # default three incorrect choices
-# return as indices?
-def incorrectAnswers(file, correctIndex, numberOfAnswers=3):
+def incorrectAnswers(file, correctIndex, lines, numberOfAnswers=3):
     with open(file) as myList:
-        output = []
-        # check that incorrect answer doesn't match correct answer
-        # only Japanese text needed for incorrect answers
+        if numberOfAnswers > len(lines) - 1:
+            numberOfAnswers = len(lines) - 1
+        indices = sample(range(0, len(lines)), numberOfAnswers)
+        count = 0
+        for i in indices:
+            while indices[count] == correctIndex:
+                newIndex = sample(range(0, len(lines)), 1)
+                if any(newIndex == j for j in indices):
+                    continue
+                else:
+                    indices[count] = newIndex[0]
+            count += 1                
+        return indices    
+        
 
 
 randomizeQuestions('GurengeBeginnerVocab.html')
