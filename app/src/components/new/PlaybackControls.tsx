86a4880259c6a3007704f4ab02ee1aab908b2f88
Joey Steele <jsteele@gatech.edu>
2021-10-29 10:46:45 -0400

    Add play/pause button to playback controls

--- app/src/components/new/PlaybackControls.tsx
+++ app/src/components/new/PlaybackControls.tsx
@@ -2,14 +2,33 @@ import React from 'react';
 
 import './PlaybackControls.css';
 
-export const PlaybackControls = () => {
+export interface Props {
+    onClick?: (playing: boolean) => void;
+}
+
+const PlaybackControls = ({ onClick }: Props) => {
+    const [playing, setPlaying] = React.useState(false);
+
+    const toggle = () => {
+        if (onClick) {
+            onClick(!playing);
+        }
+
+        setPlaying(!playing);
+    };
+
     return (
         <div className="PlaybackControls">
-            <div>Play/Pause</div>
+            <button onClick={toggle}>
+                {playing ? PAUSE_ICON : PLAY_ICON}
+            </button>
             <div>0:00 / 4:20</div>
             <div>Scrubber</div>
         </div>
     );
 };
+
+const PLAY_ICON = '\u25b6';
+const PAUSE_ICON = '\u23f8';
 
 export default PlaybackControls;
