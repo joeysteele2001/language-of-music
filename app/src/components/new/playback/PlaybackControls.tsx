e66b31843e78ef13d86f15f5e98d184487da47b6
Joey Steele <jsteele@gatech.edu>
2021-11-07 21:54:11 -0500

    Update playback controls

--- app/src/components/new/playback/PlaybackControls.tsx
+++ app/src/components/new/playback/PlaybackControls.tsx
@@ -8,40 +8,38 @@ import { Duration, Milliseconds, duration as dur } from '../../../util/duration'
 import './PlaybackControls.css';
 
 export interface Props {
+    playing?: boolean,
+    time?: number | Duration;
     duration?: number | Duration;
-    onClick?: (playing: boolean) => void;
+    onClick?: () => void;
+    onTimeScrub?: (newTime: number) => void;
 }
 
 const defaultDuration = dur.toMillis({ minutes: 4, seconds: 20 });
 
 const PlaybackControls = (props: Props) => {
-    const { onClick } = props;
+    const { playing, onClick, onTimeScrub } = props;
 
     let rawDuration = props.duration || defaultDuration;
     if (typeof rawDuration === 'object') {
         rawDuration = dur.toMillis(rawDuration);
     }
-
     const duration: Milliseconds = rawDuration;
 
-    const [playing, setPlaying] = React.useState(false);
+    let rawTime = props.time || 0;
+    if (typeof rawTime === 'object') {
+        rawTime = dur.toMillis(rawTime);
+    }
+    const time: Milliseconds = rawTime;
 
     // normalized playback time, from 0.0 to 1.0
-    const [time, setTime] = React.useState(0);
-
-    const toggle = () => {
-        if (onClick) {
-            onClick(!playing);
-        }
-
-        setPlaying(!playing);
-    };
+    const normalizedTime = time / duration;
 
     return (
         <div className="PlaybackControls">
-            <PlayPauseButton onClick={toggle} playing={playing} />
+            <PlayPauseButton onClick={onClick} playing={playing} />
             <PlaybackProgress currentTime={time * duration} duration={duration} />
-            <Scrubber value={time} onChange={setTime} />
+            <Scrubber value={normalizedTime} onChange={onTimeScrub} />
         </div>
     );
 };
