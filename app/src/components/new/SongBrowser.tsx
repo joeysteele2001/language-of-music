5dd19280f870359fd6f53e94cfdf5b05f810a973
Joey Steele <jsteele@gatech.edu>
2021-11-08 23:32:20 -0500

    Make song browser list dynamic

--- app/src/components/new/SongBrowser.tsx
+++ app/src/components/new/SongBrowser.tsx
@@ -1,52 +1,68 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export const SongBrowser = () => {
+import { Song } from '../../util/song';
+
+export interface Props {
+    songs?: Song[];
+}
+
+const DEFAULT_SONGS: Song[] = [
+    {
+        title: 'Gurenge',
+        artist: 'LiSA',
+        videoId: '_IkopJwRDKU',
+    },
+    {
+        title: 'Gurenge',
+        artist: 'LiSA',
+        videoId: '_IkopJwRDKU',
+    },
+    {
+        title: 'You Are Immune',
+        artist: 'Kurzgesagt',
+        videoId: 'LmpuerlbJu0',
+    },
+    {
+        title: 'Never Gonna Give You Up',
+        artist: 'Rick Astley',
+        videoId: 'dQw4w9WgXcQ',
+    },
+    {
+        title: 'Gurenge',
+        artist: 'LiSA',
+        videoId: '_IkopJwRDKU',
+    },
+    {
+        title: 'Gurenge',
+        artist: 'LiSA',
+        videoId: '_IkopJwRDKU',
+    },
+]
+
+export const SongBrowser = (props: Props) => {
+    const songs = props.songs || DEFAULT_SONGS;
+
     return (
         <div className="SongBrowser">
-            <div className="SongBrowser-song">
-                <Link to="#">
-                    <div className="SongBrowser-thumbnail"></div>
-                    <div className="SongBrowser-name">Song 1</div>
-                </Link>
-            </div>
-
-            <div className="SongBrowser-song">
-                <Link to={`/new-songpage?song=Vq04MeNcRUM`}>
-                    <div
-                        className="SongBrowser-thumbnail"
-                        style={{
-                            backgroundImage: "url(https://img.youtube.com/vi/_IkopJwRDKU/mqdefault.jpg)",
-                            backgroundRepeat: "no-repeat",
-                            backgroundPosition: "center",
-                            backgroundSize: "cover",
-                        }}
-                    />
-                    <div className="SongBrowser-name">Gurenge - LiSA</div>
-                </Link>
-            </div>
-
-            <div className="SongBrowser-song">
-                <Link to="#">
-                    <div className="SongBrowser-thumbnail"></div>
-                    <div className="SongBrowser-name">Song 3</div>
-                </Link>
-            </div>
-
-            <div className="SongBrowser-song">
-                <div className="SongBrowser-thumbnail"></div>
-                <Link to="#">Song 4</Link>
-            </div>
-
-            <div className="SongBrowser-song">
-                <div className="SongBrowser-thumbnail"></div>
-                <Link to="#">Song 5</Link>
-            </div>
-
-            <div className="SongBrowser-song">
-                <div className="SongBrowser-thumbnail"></div>
-                <Link to="#">Song 6</Link>
-            </div>
+            {
+                songs.map(song => (
+                    <div className="SongBrowser-song">
+                        <Link to={`/new-songpage?song=${song.videoId}`}>
+                            <div
+                                className="SongBrowser-thumbnail"
+                                style={{
+                                    backgroundImage: `url(https://img.youtube.com/vi/${song.videoId}/mqdefault.jpg)`,
+                                    backgroundRepeat: "no-repeat",
+                                    backgroundPosition: "center",
+                                    backgroundSize: "cover",
+                                }}
+                            />
+                            <div className="SongBrowser-name">{song.title}{song.artist && ` - ${song.artist}`}</div>
+                        </Link>
+                    </div>
+                ))
+            }
         </div>
     );
 };
