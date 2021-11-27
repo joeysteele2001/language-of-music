6fc940aa2781a57c0a5e40a0006ddab6092e5482
Joey Steele <jsteele@gatech.edu>
2021-11-26 21:21:03 -0500

    Add HQ letters next to HQ indicator icon

--- app/src/components/pieces/HqIndicator.tsx
+++ app/src/components/pieces/HqIndicator.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 import Icon, { IconName, IconSize } from './Icon';
 
 export const HqIndicator = () => {
-    return <Icon name={IconName.Star} size={IconSize.Large} />
+    return (
+        <>
+            <Icon name={IconName.Star} size={IconSize.Large} />&nbsp;
+            <abbr title="High-Quality Translation">HQ</abbr>
+        </>
+    );
 };
 
 export default HqIndicator;
