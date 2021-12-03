37882d76c26e9d28f7b04ad4f1ae1d3e34b34d87
Joey Steele <jsteele@gatech.edu>
2021-12-03 08:49:11 -0500

    Add translation files to lyrics

--- app/src/util/lyrics.ts
+++ app/src/util/lyrics.ts
@@ -6,6 +6,10 @@ import gurengeLyrics from '../resources/lyrics/gurenge-lisa.json';
 import yoruNiKakeruLyrics from '../resources/lyrics/yoru-ni-kakeru.json';
 import pretenderLyrics from '../resources/lyrics/pretender.json';
 
+import gurengeTrans from '../resources/lyrics/gurenge-lisa-translation.json';
+import yoruNiKakeruTrans from '../resources/lyrics/yoru-ni-kakeru-translation.json';
+import pretenderTrans from '../resources/lyrics/pretender-translation.json';
+
 export type Lyrics = { times?: Milliseconds[], lyrics: LyricsText, translation?: LyricsText };
 export type LyricsText = LyricsLine[];
 export type LyricsLine = AnnotatedText[];
@@ -21,9 +25,9 @@ const mergeLyricsAndTranslation = (lyrics: Lyrics, translation?: { translation: 
     };
 };
 
-export const GURENGE_LYRICS = mergeLyricsAndTranslation(gurengeLyrics);
-export const YORU_LYRICS = mergeLyricsAndTranslation(yoruNiKakeruLyrics);
-export const PRETENDER_LYRICS = mergeLyricsAndTranslation(pretenderLyrics);
+export const GURENGE_LYRICS = mergeLyricsAndTranslation(gurengeLyrics, gurengeTrans);
+export const YORU_LYRICS = mergeLyricsAndTranslation(yoruNiKakeruLyrics, yoruNiKakeruTrans);
+export const PRETENDER_LYRICS = mergeLyricsAndTranslation(pretenderLyrics, pretenderTrans);
 export const FALLING_LYRICS = mergeLyricsAndTranslation(fallingLyrics);
 
 // TODO: add times to Pretender and Yoru... lyrics
