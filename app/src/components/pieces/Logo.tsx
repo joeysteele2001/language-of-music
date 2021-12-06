ff622066ce5c434791fdcdb897c63f5a3ef8d2b6
Joey Steele <jsteele@gatech.edu>
2021-12-06 11:00:03 -0500

    Add logo image

--- app/src/components/pieces/Logo.tsx
+++ app/src/components/pieces/Logo.tsx
@@ -3,11 +3,16 @@ import { Link } from 'react-router-dom';
 
 import styles from './Logo.module.css';
 
+import CantioLogo from '../../resources/Cantio.png';
+
 const APP_NAME = 'Cantio';
 
 export const Logo = () => {
     return <Link className={styles.nolink} to="/">
-        <div className={styles.logo}>{APP_NAME}</div>
+        <div className={styles.logo}>
+            <img src={CantioLogo} />
+        </div>
+        {/* <div className={styles.logo}>{APP_NAME}</div> */}
     </Link>;
 };
 
