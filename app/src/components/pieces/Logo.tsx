aa458b51decab32774c78dbc5fff6e3900ed0348
Joey Steele <jsteele@gatech.edu>
2021-12-06 11:01:22 -0500

    Add alt to logo

--- app/src/components/pieces/Logo.tsx
+++ app/src/components/pieces/Logo.tsx
@@ -5,12 +5,10 @@ import styles from './Logo.module.css';
 
 import CantioLogo from '../../resources/Cantio.png';
 
-const APP_NAME = 'Cantio';
-
 export const Logo = () => {
     return <Link className={styles.nolink} to="/">
         <div className={styles.logo}>
-            <img src={CantioLogo} />
+            <img src={CantioLogo} alt="Cantio Logo" />
         </div>
         {/* <div className={styles.logo}>{APP_NAME}</div> */}
     </Link>;
